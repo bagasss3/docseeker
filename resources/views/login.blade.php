05ed4319b7680fa7222d93caa3d269ffe96ee5d0
zydane07 <muhammadzydane3@gmail.com>
2022-07-19 16:53:42 +0700

    login, checkout fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,8 +8,8 @@
 
 <!-- content -->
 <div class="container">
-    <div class="row d-flex justify-content-center">
-        <div class="col-lg-6 box-login shadow mt-4 mb-4 me-3 ms-2">
+    <div class="row d-flex justify-content-center" style="gap: 3em;">
+        <div class="col-lg-6 box-login shadow mt-4 mb-4 ms-2">
             <h1 class="fw-bold text-center">Create new account</h1>
             <p class="text-primary mt-3">Dengan mendaftar akun, Anda akan dapat melalui proses pembayaran lebih cepat, menyimpan alamat pengiriman, melihat dan melacak pesanan Anda di akun Anda dan banyak lagi.</p>
 
@@ -19,13 +19,13 @@
                 </a>
             </div>
         </div>
-        <div class="col-lg-6 box-login shadow mt-4 mb-4 me-3 ms-2">
+        <div class="col-lg-6 box-login shadow mt-4 mb-4 ms-2">
             <h1 class="fw-bold text-center">LOGIN</h1>
             @if(session()->has('info'))
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    {{ session('info') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
-                </div>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('info') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+            </div>
             @endif
             <form action="{{ route('authenticate') }}" method="POST" class=" mt-3">
                 @csrf
@@ -33,9 +33,9 @@
                     <label for="email" class="form-label" name="email">Email Address</label>
                     <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter E-mail" value="{{ old('email') }}">
                     @error('email')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
                     @enderror
                 </div>
                 <div class="mb-3">

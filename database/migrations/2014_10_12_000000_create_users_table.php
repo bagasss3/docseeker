28dc918450034b504b7360b6bc138958eb29a4dd
bagasss3 <64892035+bagasss3@users.noreply.github.com>
2022-09-14 18:51:27 +0700

    feature: cloudinary init (#32)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,17 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->enum('role_id',[0,1])->default(0);
-            $table->string('first_name',100);
-            $table->string('last_name',100);
-            $table->string('email',200)->unique();
-            $table->string('phone',15)->nullable();
-            $table->string('street_address',100)->nullable();
+            $table->enum('role_id', [0, 1])->default(0);
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('email', 200)->unique();
+            $table->string('phone', 15)->nullable();
+            $table->string('street_address', 100)->nullable();
             $table->string('password');
-            $table->string('country',50)->nullable();
-            $table->string('provice',50)->nullable();
-            $table->string('city',50)->nullable();
-            $table->string('zip_code',10)->nullable();
+            $table->string('country', 50)->nullable();
+            $table->string('provice', 50)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->string('zip_code', 10)->nullable();
+            $table->string('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

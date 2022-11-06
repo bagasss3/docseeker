14a8092e6adad6c3ebeacc5158f8fec5ad79692d
Bagas Prabaswara <bagasprabaswara77@gmail.com>
2022-11-06 10:22:59 +0700

    refactor: admin auth

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\Admin as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 

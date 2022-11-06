9aee82d01365cefac9d90707e405d0968d99a2eb
bagasss3 <64892035+bagasss3@users.noreply.github.com>
2022-11-06 10:04:52 +0700

    refactor: css js not load (#54)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('APP_ENV.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
